1e8d595e8a1625f25a5d40d97ce329d10757825d
Romuald <romuald@romuald.io>
2020-06-07 22:20:51 -0400

    Adding GitHub action definition

---
+++ entrypoint.sh
@@ -0,0 +1,20 @@
+#!bin/sh -x
+
+# Output versions.
+hugo version
+firebase --version
+
+# Build the Hugo site.
+hugo
+
+# Retrieve the input arguments/parameters.
+FIREBASE_DEPLOY_TOKEN=$1
+
+# Publish to Google Firebase.
+firebase use --token $FIREBASE_DEPLOY_TOKEN
+firebase deploy -m "
+  Successful Deployment:
+  Event: $GITHUB_EVENT_NAME,
+  Commit SHA: $GITHUB_SHA,
+  User: $GITHUB_ACTOR
+" --non-interactive --token $FIREBASE_DEPLOY_TOKEN
